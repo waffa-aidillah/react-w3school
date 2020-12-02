88ef47ba935a36ecf5461cabf1a8a96b6f33e972
Waffa Aidillah <waffaaidillah23@gmail.com>
2020-12-02 18:46:42 +0700

    life cycle

--- src/Learn.js
+++ src/Learn.js
@@ -4,8 +4,22 @@ class Learn extends Component {
   state = {
     colorr: 'red',
   };
-  changeState=()=>{
-      this.setState({colorr:'blue'})
+  changeState = () => {
+    this.setState({ colorr: 'blue' });
+  };
+  //*method yg dipanggil tepat sebelum rendering
+  //*untuk set state di objek
+  //*dibawah ini aslinya state warna merah, tapi diubah jadi biru
+  /*
+  static getDerivedStateFromProps(props, state) {
+    return { colorr: props.brand.model };
+  }
+  */
+  //*method yang dipanggil setelah component terender
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ colorr: 'green' });
+    }, 2000);
   }
   render() {
     let styling = {
@@ -16,12 +30,11 @@ class Learn extends Component {
     return (
       <div className='class1' style={styling}>
         <h1 style={colorStyle}>hello</h1>
-    <h1>ini adalah state: {this.state.colorr}</h1>
+        <h1>ini adalah state: {this.state.colorr}</h1>
         <h2>ini adalah prop brand model {this.props.brand.model}</h2>
-        <button type='button'
-            onClick={this.changeState}
-        >ubah state</button>
-
+        <button type='button' onClick={this.changeState}>
+          ubah state
+        </button>
       </div>
     );
   }
